b377ce592099a1df1a422a5c29999a6f38ec89f0
Anna <ankakras@yandex.ru>
2022-11-29 16:16:02 +0700

    Закончила решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,6 +1,20 @@
 ﻿string[] text = {"Hello","World","!",":-)"};
+int n = 3; //максимальная длина элемента
 int count = 0;
 for(int i = 0; i < text.Length; i++)
 {
-    if(text[i].Length <= 3) count++;
+    if(text[i].Length <= n) count++;
 }
+string[] text2 = new string[count];
+int j = 0;
+for(int i = 0; i < text.Length; i++)
+{
+    if(text[i].Length <= n)
+    {
+        text2[j] = text[i];
+        j++;
+    }
+}
+for(int i = 0; i < text2.Length; i++)
+    Console.Write($"{text2[i]} ");
+Console.WriteLine();
